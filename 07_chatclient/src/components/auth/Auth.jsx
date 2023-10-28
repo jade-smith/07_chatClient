b1bdf134ee8aeb9ad51f930b86826cb3fc17167f
JFEdski <jedelinski@gmail.com>
2023-10-28 15:21:00 -0600

    Auth and Signup for Ticket #U6_02

---
+++ 07_chatclient/src/components/auth/Auth.jsx
@@ -0,0 +1,18 @@
+import Signup from "./signup/Signup";
+import Login from "./login/Login";
+
+function Auth(props) {
+    return (
+        <>
+        <div style={{ width: '50%', display: 'inline-block' }}>
+        <h2>Login</h2>
+        <Login setToken={props.setToken} />
+        </div>
+        <div style={{ width: '50%', display: 'inline-block' }}>
+            <Signup setToken={props.setToken} />
+        </div>
+        </>
+    )
+}
+
+export default Auth;
---
+++ 07_chatclient/src/components/auth/signup/Signup.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Signup(props) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [userName, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [verifyPassword, setVerifyPassword] = useState('');
+  const signupRoute = 'http://127.0.0.1:4000/user/signup'
+
+  return (
+    <div>
+      <form>
+        <input placeholder="email" onChange={ e => setEmail(e.target.value) } />
+
+        <input placeholder="username" onChange={ e => setUser(e.target.value) } />
+
+        <input placeholder="password" onChange={ e => setPassword(e.target.value) } />
+
+        <input placeholder="verify password" onChange={ e => setVerifyPassword(e.target.value) } />
+
+        <button type="submit" onClick={displayInputFields}>Submit</button>
+      </form>
+    </div>
+  )
+
+  async function displayInputFields(e) {
+    e.preventDefault();
+    console.log('Test Function');
+    console.log(userName);
+    console.log(email);
+    console.log(password);
+    if (password === verifyPassword) {
+      try {
+        let response = await fetch(signupRoute, {
+          headers: new Headers({
+            'content-type': 'application/json'
+          }),
+          method: 'POST',
+          body: JSON.stringify({
+            mail: email,
+            user: userName,
+            pass: password,
+          })
+        })
+  
+        let results = await response.json();
+        console.log(results);
+        props.setToken(results.token);
+        if(response.status === 200)
+          navigate('/room');
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  }
+}
+
+function DisplayUser(props) {
+  return (
+    <div>
+      <h2>Username: { props.userName }</h2>
+      <h2>Password: { props.password }</h2>
+    </div>
+  )
+}
+
+export default Signup
